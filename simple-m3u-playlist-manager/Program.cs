14663aea5542ba1801ace951180344abf5bf2b5d
Laurens Panier <ozafy@borknet.org>
2023-05-10 11:20:07 +0200

    started

--- simple-m3u-playlist-manager/Program.cs
+++ simple-m3u-playlist-manager/Program.cs
@@ -1,2 +1,34 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using simple_m3u_playlist_manager;
+using System.IO;
+
+Console.WriteLine("This program will create m3u files in the selected directory and subdirectories containing all music files found.");
+var currentDir = Directory.GetCurrentDirectory();
+Console.WriteLine($"Starting directory [{currentDir}]:");
+var dir = Console.ReadLine();
+if (string.IsNullOrWhiteSpace(dir)) {
+    dir = currentDir;
+}
+var removeOld = "";
+while (removeOld != "y" && removeOld != "n") {
+    Console.WriteLine($"Remove existing m3u files (Y/N) [Y]:");
+    removeOld = Console.ReadLine().ToLower();
+    if (string.IsNullOrWhiteSpace(dir)) {
+        removeOld = "y";
+    }
+}
+var basefileFilter = "mp3,flac,wav,wma,aac,ogg";
+Console.WriteLine($"Files extentions to add, comma seperated [{basefileFilter}]:");
+string fileFilter = Console.ReadLine().ToLower();
+if (string.IsNullOrWhiteSpace(fileFilter)) {
+    fileFilter = basefileFilter;
+}
+string[] fileTypes = fileFilter.Split(',');
+if (Directory.Exists(dir)) {
+    Scanner.Scan(dir, fileTypes);
+    // scan directories for music and m3u files
+    // create new m3u files with directory name as playlist name in each directory (incl top)
+} else {
+    Console.WriteLine($"'{dir}' is not a valid directory");
+}
+Console.WriteLine($"Press any key to exit");
+var tmp = Console.ReadLine();
---
+++ simple-m3u-playlist-manager/Scanner.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace simple_m3u_playlist_manager {
+
+    internal class Scanner {
+
+        public static void Scan(string dir, string[] fileTypes) {
+            var musicFiles = Directory.EnumerateFiles(dir, "*.*").Where(x => fileTypes.Contains(Path.GetExtension(x).ToLower().Substring(1)));
+
+            foreach (string d in Directory.GetDirectories(dir)) {
+                Scan(d, fileTypes);
+            }
+        }
+    }
+}
